7ec66ff58e87b529788b1d8ae01d194f2e56273a
ciecclm <clm@imnu.edu.cn>
2017-02-27 11:49:28 +0800

    Initial Commit

---
+++ testswift0227/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testswift0227
+//
+//  Created by admin on 2017/2/27.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
